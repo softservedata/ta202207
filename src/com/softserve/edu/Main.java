7a0cd2e486c9aa9c59756d6b6b20c844d7925c36
Jaroslav <garasym@gmail.com>
2022-07-25 10:12:17 +0300

    Message Updated

--- src/com/softserve/edu/Main.java
+++ src/com/softserve/edu/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         //
-        System.out.print("Hello. What is your name? ");
+        System.out.print("Hello all. What is your name? ");
         String name = br.readLine();
         System.out.print("How old are you? ");
         int age = Integer.parseInt(br.readLine());
