503de5eb49de32e21a3ce6e647d136c9812f37cf
SvitlanaFirsova <sfirso@softserveinc.com>
2022-07-26 09:18:29 +0300

    my branch

--- src/com/softserve/edu/Main.java
+++ src/com/softserve/edu/Main.java
@@ -7,15 +7,13 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        //
+
         System.out.print("Hello. What is your name? ");
         String name = br.readLine();
         System.out.print("How old are you? ");
         int age = Integer.parseInt(br.readLine());
-        //
-        System.out.println("Hello " + name);
-        System.out.println("You are " + age);
-        //
-        br.close();
+
+        System.out.print("Hello " + name);
+        System.out.print(" you are " + age);
     }
 }
