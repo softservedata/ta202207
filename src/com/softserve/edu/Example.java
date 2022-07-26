a3bf86d346be7e47f244e668e3275414b74139ec
Yuliia Terentieva <yteren@softserveinc.com>
2022-07-26 14:28:58 +0200

    commit A, A, Example

---
+++ src/com/softserve/edu/Example.java
@@ -0,0 +1,48 @@
+package com.softserve.edu;
+
+import com.softserve.edu1.A;
+
+import java.util.Scanner;
+
+public class Example {
+
+    private int k = 123;
+    private float k2 = 123;
+
+    public Example() {
+        k = 456;
+    }
+
+    public Example(float k2) {
+        this.k2 = k2;
+    }
+
+    public Example(int k1) {
+        k = k1;
+    }
+
+    public static void main(String[] args) {
+        Example example = new Example();
+        System.out.println("Hello2");
+        int i = 1;
+        String s = "abc"; // s =new String("abc")
+        System.out.println("example.k = " + example.k);
+        //
+        Example example2 = new Example(789);
+        System.out.println("example2.k = " + example2.k);
+        //
+        example.k = 135;
+        example2.k = 246;
+        System.out.println("example.k = " + example.k + " example2.k = " + example2.k);
+        //
+        A a = new A();
+        a.m1();
+        com.softserve.edu2.A a2 = new com.softserve.edu2.A();
+        a2.m1();
+        //
+        Scanner sc = new Scanner(System.in);
+        System.out.print("name = ");
+        String name = sc.nextLine();
+        System.out.println("Hello " + name);
+    }
+}
---
+++ src/com/softserve/edu1/A.java
@@ -0,0 +1,7 @@
+package com.softserve.edu1;
+
+public class A {
+    public void m1(){
+        System.out.println("m1() fom edu1.A");
+    }
+}
