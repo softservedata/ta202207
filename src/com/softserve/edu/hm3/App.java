0a00bf6f0e9d8f64116ffa41d5237c9777093a95
SvitlanaFirsova <sfirso@softserveinc.com>
2022-07-30 21:59:24 +0300

    added hm3

---
+++ src/com/softserve/edu/hm3/App.java
@@ -0,0 +1,11 @@
+package com.softserve.edu.hm3;
+
+public class App {
+    public static void main(String[] args) {
+
+        Pair pair = new Pair(375, 486);
+        pair.print();
+        pair.swap();
+        pair.print();
+    }
+}
---
+++ src/com/softserve/edu/hm3/Pair.java
@@ -0,0 +1,24 @@
+package com.softserve.edu.hm3;
+
+public class Pair {
+    private int a;
+    private int b;
+
+    public Pair(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public void print() {
+        System.out.println("print a value = " + a);
+        System.out.println("print b value = " + b);
+    }
+
+    public void swap() {
+        System.out.println("a and b have been swapped");
+        int temp;
+        temp = a;
+        a = b;
+        b = temp;
+    }
+}
