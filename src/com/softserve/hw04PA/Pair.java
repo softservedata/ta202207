ce58e04c764c472faf00814328aa305662ed2ced
PA6183 <al.foero@gmail.com>
2022-07-31 22:02:31 +0300

    Added Class Pair

---
+++ src/com/softserve/hw04PA/Pair.java
@@ -0,0 +1,12 @@
+package com.softserve.hw04PA;
+
+public class Pair {
+    private int i;
+    private int k;
+
+    public Pair (int i,int k)
+    {
+    System.out.print("i = " + i);
+    System.out.println("k = " + k);
+    }
+}
