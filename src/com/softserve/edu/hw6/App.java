43cf582de136cea920f8973f7085481a6f4f4ed1
SvitlanaFirsova <sfirso@softserveinc.com>
2022-08-07 13:26:55 +0300

    hw6

---
+++ src/com/softserve/edu/hw6/App.java
@@ -0,0 +1,10 @@
+package com.softserve.edu.hw6;
+
+public class App {
+    public static void main(String[] args) {
+
+        for (Drinks drink : Drinks.values()) {
+            System.out.println("Drink is = " + drink); // current.toString()
+        }
+    }
+}
