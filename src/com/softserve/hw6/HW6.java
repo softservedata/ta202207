2174c6ada6c8bf37905b3d7b5cbad861d09ffb32
Anastasiia <nastenakv3@gmail.com>
2022-08-07 17:05:43 +0300

    Homework 6 (Enum Drinks)

---
+++ src/com/softserve/hw6/HW6.java
@@ -0,0 +1,13 @@
+package com.softserve.hw6;
+
+import java.util.EnumSet;
+
+public class HW6 {
+    public static void main(String[] args) {
+        for (Drinks drinks: EnumSet.allOf(Drinks.class)) {
+            System.out.println(drinks);
+        }
+    }
+
+    }
+
