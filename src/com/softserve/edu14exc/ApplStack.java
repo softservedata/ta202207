cea2a2b58e3fa109a37ebb072c40f8395b26b824
Jaroslav <garasym@gmail.com>
2022-09-05 09:47:10 +0300

    Exceptions. StackTrace

--- src/com/softserve/edu14exc/ApplStack.java
+++ src/com/softserve/edu14exc/ApplStack.java
@@ -1,5 +1,7 @@
 package com.softserve.edu14exc;
 
+import java.util.Arrays;
+
 class MyException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 
@@ -23,13 +25,20 @@ public class ApplStack {
 	}
 
 	public static void main(String[] args) {
-		// method1();
+		 //method1();
 		try {
 			method1();
 		} catch (Exception e) {
 			// System.err.println(e.getMessage() + "\n");
 			System.out.println(e.getMessage() + "\n");
 			e.printStackTrace();
+			//
+			String msg = "";
+			for ( StackTraceElement current : e.getStackTrace()) {
+				msg = msg + "\n\t at " + current.toString();
+			}
+			throw new RuntimeException(msg);
+			//throw new RuntimeException(Arrays.toString(e.getStackTrace()));
 		}
 
 	}
